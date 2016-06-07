c6988029f7953a4f3fc6ad94d4c8a84bfe8ed3e4
tianrui0717 <1156184421@qq.com>
2016-06-07 22:29:31 +0800

    testgit

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -2,7 +2,7 @@
 function collect_same_elements(collection_a, object_b) {
  //在此处写代码
 	var object_b = {value: ["a", "d", "e", "f"]};
-	return (["a", "f", "f", "e", "d"]);
+	return (["a", "e", "f", "d"]);
 }
 
 module.exports = collect_same_elements;
